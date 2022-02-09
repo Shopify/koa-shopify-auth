22f0a77d8baf62f5c94df874f7ed35b679941cdb
Elizbar Yousuf <elizbaryusuf@gmail.com>
2022-02-09 14:02:44 +0400

    the current session is taken from validateAuthCallback

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,43 +1,44 @@
-import {Context} from 'koa';
+import { Context } from "koa";
 
-import {OAuthStartOptions, AccessMode, NextFunction} from '../types';
+import { OAuthStartOptions, AccessMode, NextFunction } from "../types";
 
-import getCookieOptions from './cookie-options';
-import createEnableCookies from './create-enable-cookies';
-import createTopLevelOAuthRedirect from './create-top-level-oauth-redirect';
-import createRequestStorageAccess from './create-request-storage-access';
-import setUserAgent from './set-user-agent';
+import getCookieOptions from "./cookie-options";
+import createEnableCookies from "./create-enable-cookies";
+import createTopLevelOAuthRedirect from "./create-top-level-oauth-redirect";
+import createRequestStorageAccess from "./create-request-storage-access";
+import setUserAgent from "./set-user-agent";
 
-import Shopify from '@shopify/shopify-api';
+import Shopify from "@shopify/shopify-api";
 
-const DEFAULT_MYSHOPIFY_DOMAIN = 'myshopify.com';
-export const DEFAULT_ACCESS_MODE: AccessMode = 'online';
+const DEFAULT_MYSHOPIFY_DOMAIN = "myshopify.com";
+export const DEFAULT_ACCESS_MODE: AccessMode = "online";
 
-export const TOP_LEVEL_OAUTH_COOKIE_NAME = 'shopifyTopLevelOAuth';
-export const TEST_COOKIE_NAME = 'shopifyTestCookie';
-export const GRANTED_STORAGE_ACCESS_COOKIE_NAME = 'shopify.granted_storage_access';
+export const TOP_LEVEL_OAUTH_COOKIE_NAME = "shopifyTopLevelOAuth";
+export const TEST_COOKIE_NAME = "shopifyTestCookie";
+export const GRANTED_STORAGE_ACCESS_COOKIE_NAME =
+  "shopify.granted_storage_access";
 
-function hasCookieAccess({cookies}: Context) {
+function hasCookieAccess({ cookies }: Context) {
   return Boolean(cookies.get(TEST_COOKIE_NAME));
 }
 
-function grantedStorageAccess({cookies}: Context) {
+function grantedStorageAccess({ cookies }: Context) {
   return Boolean(cookies.get(GRANTED_STORAGE_ACCESS_COOKIE_NAME));
 }
 
-function shouldPerformInlineOAuth({cookies}: Context) {
+function shouldPerformInlineOAuth({ cookies }: Context) {
   return Boolean(cookies.get(TOP_LEVEL_OAUTH_COOKIE_NAME));
 }
 
 export default function createShopifyAuth(options: OAuthStartOptions) {
   const config = {
-    prefix: '',
+    prefix: "",
     myShopifyDomain: DEFAULT_MYSHOPIFY_DOMAIN,
     accessMode: DEFAULT_ACCESS_MODE,
     ...options,
   };
 
-  const {prefix} = config;
+  const { prefix } = config;
 
   const oAuthStartPath = `${prefix}/auth`;
   const oAuthCallbackPath = `${oAuthStartPath}/callback`;
@@ -45,7 +46,7 @@ export default function createShopifyAuth(options: OAuthStartOptions) {
   const inlineOAuthPath = `${prefix}/auth/inline`;
   const topLevelOAuthRedirect = createTopLevelOAuthRedirect(
     Shopify.Context.API_KEY,
-    inlineOAuthPath,
+    inlineOAuthPath
   );
 
   const enableCookiesPath = `${oAuthStartPath}/enable_cookies`;
@@ -75,13 +76,13 @@ export default function createShopifyAuth(options: OAuthStartOptions) {
         ctx.throw(400);
       }
 
-      ctx.cookies.set(TOP_LEVEL_OAUTH_COOKIE_NAME, '', getCookieOptions(ctx));
+      ctx.cookies.set(TOP_LEVEL_OAUTH_COOKIE_NAME, "", getCookieOptions(ctx));
       const redirectUrl = await Shopify.Auth.beginAuth(
         ctx.req,
         ctx.res,
         shop,
         oAuthCallbackPath,
-        config.accessMode === 'online'
+        config.accessMode === "online"
       );
       ctx.redirect(redirectUrl);
       return;
@@ -94,21 +95,22 @@ export default function createShopifyAuth(options: OAuthStartOptions) {
 
     if (ctx.path === oAuthCallbackPath) {
       try {
-        await Shopify.Auth.validateAuthCallback(ctx.req, ctx.res, ctx.query);
-
-        ctx.state.shopify = await Shopify.Utils.loadCurrentSession(ctx.req, ctx.res, config.accessMode === 'online');
+        ctx.state.shopify = await Shopify.Auth.validateAuthCallback(
+          ctx.req,
+          ctx.res,
+          ctx.query
+        );
 
         if (config.afterAuth) {
           await config.afterAuth(ctx);
         }
-      }
-      catch (e) {
+      } catch (e) {
         switch (true) {
-          case (e instanceof Shopify.Errors.InvalidOAuthError):
+          case e instanceof Shopify.Errors.InvalidOAuthError:
             ctx.throw(400, e.message);
             break;
-          case (e instanceof Shopify.Errors.CookieNotFound):
-          case (e instanceof Shopify.Errors.SessionNotFound):
+          case e instanceof Shopify.Errors.CookieNotFound:
+          case e instanceof Shopify.Errors.SessionNotFound:
             // This is likely because the OAuth session cookie expired before the merchant approved the request
             ctx.redirect(`${oAuthStartPath}?shop=${ctx.query.shop}`);
             break;
@@ -129,4 +131,4 @@ export default function createShopifyAuth(options: OAuthStartOptions) {
   };
 }
 
-export {default as Error} from './errors';
+export { default as Error } from "./errors";
